192e0e1e1c8df93284635a596a429c7a0349d658
Tushar@8640 <imtantushar44@gmail.com>
2023-08-07 16:34:36 +0600

    mobile device responsive fixed

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -17,23 +17,23 @@ function SidebarNav() {
   };
   return (
     <div className="h-full p-3 space-y-2  text-gray-800">
-      <div className="flex items-center p-2 space-x-4">
-        <img
-          src="https://source.unsplash.com/100x100/?portrait"
-          alt=""
-          className="w-12 h-12 rounded-full bg-gray-500"
-        />
-        <div>
-          <h2 className="text-lg font-semibold">{user?.name}</h2>
-          <span className="flex items-center space-x-1">
-            <Link to="/" className="text-xs hover:underline text-gray-600">
-              View profile
-            </Link>
-          </span>
+      <div className="grid grid-cols-3  divide-y divide-gray-300">
+        <div className="flex items-center p-2 space-x-4 col-span-2 md:col-span-3 order-1 md:order-1">
+          <img
+            src="https://source.unsplash.com/100x100/?portrait"
+            alt=""
+            className="w-12 h-12 rounded-full bg-gray-500"
+          />
+          <div>
+            <h2 className="text-sm font-semibold">{user?.name}</h2>
+            <span className="flex items-center space-x-1">
+              <Link to="/" className="text-xs hover:underline text-gray-600">
+                View profile
+              </Link>
+            </span>
+          </div>
         </div>
-      </div>
-      <div className="divide-y divide-gray-300">
-        <ul className="pt-2 pb-4 space-y-1 text-sm flex justify-center items-center md:flex-col">
+        <ul className="pt-2 pb-4 space-y-1 text-sm flex justify-center items-center md:flex-col col-span-3 order-3 mt-3 md:mt-0 md:order-2">
           {data?.categories.map((d) => (
             <li key={d.id} className=" text-gray-900">
               <button
@@ -51,7 +51,7 @@ function SidebarNav() {
             </li>
           ))}
         </ul>
-        <ul className="pt-4 pb-2 space-y-1 text-sm">
+        <ul className="pt-4 pb-2 space-y-1 text-sm col-span-1 md:col-span-1 order-2 md:order-3">
           <li>
             <button
               onClick={() => handleLogOut()}
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,12 +4,12 @@ import Todo from "../components/Todo";
 
 const Dashboard = () => {
   return (
-    <div className="grid place-items-center h-screen">
-      <div className=" max-w-6xl h-[700px] bg-white p-2  md:border-[3px] md:border-gray-600/[.4] rounded-3xl">
+    <div className="grid md:place-items-center h-screen">
+      <div className=" max-w-6xl h-[700px] bg-white p-2  md:border-[3px] md:border-gray-600/[.4] rounded-3xl w-full">
         <Header />
 
-        <div className="grid grid-cols-12">
-          <div className="md:col-span-2 col-span-12">
+        <div className="grid grid-cols-12 ">
+          <div className="md:col-span-2 col-span-12 ">
             <SidebarNav />
           </div>
           <div className="col-span-12 md:col-span-10 mx-5">
